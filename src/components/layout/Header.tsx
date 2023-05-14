5bbc09198ce51388fd8f3e0088d53c83ee740ccf
Daniel Way <projects@danieldway.com>
2023-05-14 07:57:53 -0500

    Improve date formatting

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,12 +1,21 @@
 import { AppBar, Toolbar, Typography } from "@mui/material";
 
+const dateOptions: Intl.DateTimeFormatOptions = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+};
+
 export const Header = () => (
   <AppBar position="static">
     <Toolbar>
       <Typography variant="h5" sx={{ flexGrow: 1 }}>
         Task Timer
       </Typography>
-      <Typography>{new Date().toDateString()}</Typography>
+      <Typography>
+        {new Date().toLocaleDateString("en-US", dateOptions)}
+      </Typography>
     </Toolbar>
   </AppBar>
 );
