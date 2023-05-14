d68ece415af38fb2065a5ea4db85704048f9ee8f
Daniel Way <projects@danieldway.com>
2023-05-14 09:11:19 -0500

    Add version placeholder in footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,8 +3,11 @@ import { Box, Divider, Link } from "@mui/material";
 export const Footer = () => (
   <Box>
     <Divider variant="middle" />
-    <Box my={2} fontSize={13} textAlign="center">
+    <Box mt={2} fontSize={13} textAlign="center">
       Created by {websiteLink()} under the MIT License. {repositoryLink()}
+    </Box>
+    <Box mb={2} fontSize={12} textAlign="center">
+      {releaseLink()}
     </Box>
   </Box>
 );
@@ -18,5 +21,14 @@ const websiteLink = () => (
 const repositoryLink = () => (
   <Link href="https://github.com/danielway/task-timer" color="secondary">
     Contribute or fork on GitHub.
+  </Link>
+);
+
+const releaseLink = () => (
+  <Link
+    href="https://github.com/danielway/task-timer/releases/tag/REPLACE_WITH_RELEASE"
+    color="secondary"
+  >
+    vREPLACE_WITH_RELEASE
   </Link>
 );
