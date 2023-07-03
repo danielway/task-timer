a82af09c6c91369bd858f058b142d13525623c28
Daniel Way <projects@danieldway.com>
2023-07-03 17:48:41 -0500

    Add localStorage state persistence

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import task from "./slice";
 
+const persistedStateJson = localStorage.getItem("state");
+const persistedState = persistedStateJson
+  ? JSON.parse(persistedStateJson)
+  : null;
+
 export const store = configureStore({
+  preloadedState: persistedState,
   reducer: {
     tasks: task,
   },
+  middleware: (getDefaultMiddleware) => [
+    ...getDefaultMiddleware(),
+    (store) => (next) => (action) => {
+      next(action);
+
+      const state = store.getState();
+      localStorage.setItem("state", JSON.stringify(state));
+    },
+  ],
 });
 
 export type RootState = ReturnType<typeof store.getState>;
