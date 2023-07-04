b20635ab9d894a24aa0c0c9f358a87463974dc29
Daniel Way <projects@danieldway.com>
2023-07-04 13:20:11 -0500

    Extract table body into separate component

--- src/components/layout/Table.tsx
+++ src/components/layout/Table.tsx
@@ -3,36 +3,21 @@ import {
   Table as MuiTable,
   TableHead,
   TableRow,
-  TableBody,
   Paper,
   TableCell,
 } from "@mui/material";
-import { TaskCreationRow } from "../task/TaskCreationRow";
-import { TaskRow } from "../task/TaskRow";
 import { TimeHeaderCells } from "../time/TimeHeaderCells";
-import { useAppDispatch, useAppSelector } from "../../app/hooks";
-import {
-  createTask,
-  deleteTask,
-  logTime,
-  removeTime,
-  selectDates,
-  selectTasks,
-  selectTimes,
-  updateTask,
-} from "../../app/slice";
+import { useAppSelector } from "../../app/hooks";
+import { selectDates, selectTasks } from "../../app/slice";
 import { TimeCursor } from "../time/TimeCursor";
 import { useCallback, useEffect, useRef, useState } from "react";
+import { TableBody } from "./TableBody";
 
 export const Table = () => {
-  const dispatch = useAppDispatch();
-
   const dates = useAppSelector(selectDates);
   const selectedDate = dates[1];
 
   const tasks = useAppSelector((state) => selectTasks(state, selectedDate));
-  const times = useAppSelector((state) => selectTimes(state, selectedDate));
-
   const tableRef = useRef<HTMLTableElement>(null);
 
   const [timeHeight, setTimeHeight] = useState<number>(0);
@@ -80,35 +65,7 @@ export const Table = () => {
             <TableCell className="header">Total</TableCell>
           </TableRow>
         </TableHead>
-        <TableBody>
-          {tasks.map((task) => (
-            <TaskRow
-              task={task}
-              time={times.filter((time) => time.taskId === task.id)}
-              key={task.id}
-              updateTask={(id, name) =>
-                dispatch(updateTask({ date: selectedDate, id, name }))
-              }
-              deleteTask={(task) =>
-                dispatch(deleteTask({ date: selectedDate, task }))
-              }
-              logTime={(taskId, timeSegment) =>
-                dispatch(logTime({ date: selectedDate, taskId, timeSegment }))
-              }
-              removeTime={(taskId, timeSegment) =>
-                dispatch(
-                  removeTime({ date: selectedDate, taskId, timeSegment })
-                )
-              }
-            />
-          ))}
-          <TaskCreationRow
-            timeCount={times.length}
-            createTask={(name) =>
-              dispatch(createTask({ date: selectedDate, name }))
-            }
-          />
-        </TableBody>
+        <TableBody />
       </MuiTable>
     </TableContainer>
   );
---
+++ src/components/layout/TableBody.tsx
@@ -0,0 +1,54 @@
+import { TableBody as MuiTableBody } from "@mui/material";
+import { TaskCreationRow } from "../task/TaskCreationRow";
+import { TaskRow } from "../task/TaskRow";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import {
+  createTask,
+  deleteTask,
+  logTime,
+  removeTime,
+  selectDates,
+  selectTasks,
+  selectTimes,
+  updateTask,
+} from "../../app/slice";
+
+export const TableBody = () => {
+  const dispatch = useAppDispatch();
+
+  const dates = useAppSelector(selectDates);
+  const selectedDate = dates[1];
+
+  const tasks = useAppSelector((state) => selectTasks(state, selectedDate));
+  const times = useAppSelector((state) => selectTimes(state, selectedDate));
+
+  return (
+    <MuiTableBody>
+      {tasks.map((task) => (
+        <TaskRow
+          task={task}
+          time={times.filter((time) => time.taskId === task.id)}
+          key={task.id}
+          updateTask={(id, name) =>
+            dispatch(updateTask({ date: selectedDate, id, name }))
+          }
+          deleteTask={(task) =>
+            dispatch(deleteTask({ date: selectedDate, task }))
+          }
+          logTime={(taskId, timeSegment) =>
+            dispatch(logTime({ date: selectedDate, taskId, timeSegment }))
+          }
+          removeTime={(taskId, timeSegment) =>
+            dispatch(removeTime({ date: selectedDate, taskId, timeSegment }))
+          }
+        />
+      ))}
+      <TaskCreationRow
+        timeCount={times.length}
+        createTask={(name) =>
+          dispatch(createTask({ date: selectedDate, name }))
+        }
+      />
+    </MuiTableBody>
+  );
+};
