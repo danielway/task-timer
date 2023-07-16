cb233af667d1d378fa73d426bbe8b61069068e40
Daniel Way <projects@danieldway.com>
2023-07-16 12:33:29 -0500

    Update slices in store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,23 +1,35 @@
 import { configureStore } from "@reduxjs/toolkit";
-import task from "./slice";
+import app from "./appSlice";
+import edit from "./editSlice";
+import date from "./dateSlice";
+import task from "./taskSlice";
+import time from "./timeSlice";
 
-const persistedStateJson = localStorage.getItem("state");
-const persistedState = persistedStateJson
-  ? JSON.parse(persistedStateJson)
-  : undefined;
+const loadState = () => {
+  const persistedStateJson = localStorage.getItem("state");
+  const persistedState = persistedStateJson
+    ? JSON.parse(persistedStateJson)
+    : undefined;
+
+  // Future migrations go here
+
+  return persistedState;
+};
+
+const saveState = (state: RootState) => {
+  localStorage.setItem("state", JSON.stringify(state));
+};
 
 export const store = configureStore({
-  preloadedState: persistedState,
-  reducer: {
-    tasks: task,
-  },
+  preloadedState: loadState(),
+  reducer: { app, edit, date, task, time },
   middleware: (getDefaultMiddleware) => [
     ...getDefaultMiddleware(),
     (store) => (next) => (action) => {
       next(action);
 
       const state = store.getState();
-      localStorage.setItem("state", JSON.stringify(state));
+      saveState(state);
     },
   ],
 });
